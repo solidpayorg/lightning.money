3825d50f6c3ef439133365a5ef414161be435bb7
Melvin Carvalho <melvincarvalho@gmail.com>
2019-11-05 22:22:20 +0100

    Add lightning wiki

--- index.mm
+++ index.mm
@@ -143,6 +143,7 @@
 <font BOLD="true"/>
 <node TEXT="lopp list" ID="ID_1897426799" CREATED="1569941168323" MODIFIED="1572329436695" LINK="https://www.lopp.net/lightning-information.html"/>
 <node TEXT="awesome list" ID="ID_1244510370" CREATED="1569861424912" MODIFIED="1572329447313" LINK="https://github.com/bcongdon/awesome-lightning-network"/>
+<node TEXT="lightning wiki" ID="ID_806544715" CREATED="1572988881428" MODIFIED="1572988891410" LINK="https://lightningwiki.net/index.php/Main_Page"/>
 <node TEXT="lnroute" ID="ID_1451851808" CREATED="1570185749629" MODIFIED="1570185757230" LINK="https://lnroute.com/"/>
 </node>
 <node TEXT="communities" ID="ID_1721453071" CREATED="1572852581179" MODIFIED="1572852785776">
