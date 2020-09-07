8e08b718e9c7acadcefafb687ae1edeec3e0fe19
Melvin Carvalho <melvincarvalho@gmail.com>
2020-09-07 20:33:26 +0200

    add etleneum

--- contracts/etleneum/index.mm
+++ contracts/etleneum/index.mm
@@ -63,6 +63,49 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="apps" POSITION="right" ID="ID_155456580" CREATED="1599502609475" MODIFIED="1599502940454">
+<icon BUILTIN="bookmark"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="Predictions" ID="ID_230763179" CREATED="1599502647098" MODIFIED="1599502916964" LINK="https://predictions.etleneum.com/">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Chess" ID="ID_623666999" CREATED="1599502819185" MODIFIED="1599503083431" LINK="http://lichess.etleneum.com/">
+<icon BUILTIN="emoji-265F"/>
+</node>
+<node TEXT="Chain Market" ID="ID_97132608" CREATED="1599502840947" MODIFIED="1599502860224" LINK="https://chainmarket.etleneum.com/">
+<icon BUILTIN="list"/>
+</node>
+<node TEXT="Banner Ads" ID="ID_1128227880" CREATED="1599502681218" MODIFIED="1599502699207" LINK="https://banners.etleneum.com/">
+<icon BUILTIN="info"/>
+</node>
+<node TEXT="Keybase" ID="ID_775382578" CREATED="1599502870162" MODIFIED="1599502882120" LINK="https://kad.etleneum.com/">
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="Answers" ID="ID_102962202" CREATED="1599502710906" MODIFIED="1599502722177" LINK="https://s4a.etleneum.com/">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node TEXT="Links" POSITION="left" ID="ID_1589865073" CREATED="1599502907554" MODIFIED="1599502912014">
+<edge COLOR="#0000ff"/>
+<node TEXT="Site" ID="ID_1240101594" CREATED="1599502923073" MODIFIED="1599502931064" LINK="etleneum.com">
+<icon BUILTIN="wizard"/>
+</node>
+<node TEXT="Docs" ID="ID_1094040580" CREATED="1599502943361" MODIFIED="1599503126191" LINK="https://etleneum.com/#/docs">
+<icon BUILTIN="emoji-1F4D4"/>
+</node>
+<node TEXT="Test" ID="ID_1572353532" CREATED="1599503136213" MODIFIED="1599503182590" LINK="https://test.etleneum.com/">
+<icon BUILTIN="emoji-1F528"/>
+</node>
+<node TEXT="Social" ID="ID_741069104" CREATED="1599503208326" MODIFIED="1599503226492">
+<icon BUILTIN="fema"/>
+<node TEXT="Telegram" ID="ID_1447316975" CREATED="1599503227885" MODIFIED="1599503485837" LINK="https://t.me/etleneum"/>
+<node TEXT="Twitter" ID="ID_1489373962" CREATED="1599503478840" MODIFIED="1599503548728" LINK="https://twitter.com/etleneum2"/>
+</node>
+</node>
+<node TEXT="Contracts" POSITION="right" ID="ID_1943988554" CREATED="1599502980024" MODIFIED="1599503002917" LINK="https://etleneum.com/#/contracts">
+<icon BUILTIN="edit"/>
+<edge COLOR="#00ff00"/>
+</node>
 </node>
 </map>
