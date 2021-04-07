e09c2ffb526214844d068456713ba316186aed09
Melvin Carvalho <melvincarvalho@gmail.com>
2021-04-07 10:58:16 +0200

    cleanup

--- bin/tx.sh
+++ bin/tx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-RAWTX=$(bitmark-cli createrawtransaction '[{"txid": "'$1'", "vout": 0}]' '{"'$2'": '$3'}')
+RAWTX=$(bitmark-cli createrawtransaction '[{"txid": "'$1'", "vout": $2}]' '{"'$3'": '$4'}')
 
-SENDTX=$(bitmark-cli signrawtransaction $RAWTX null '["'$4'"]' | grep ffff | sed 's/.*: "\(.*\)".*/\1/')
+SENDTX=$(bitmark-cli signrawtransaction $RAWTX null '["'$5'"]' | grep ffff | sed 's/.*: "\(.*\)".*/\1/')
 
-bitmark-cli sendrawtransaction $SENDTX
+NEWTX=$(bitmark-cli sendrawtransaction $SENDTX)
+
+echo $NEWTX
+
+echo "git commit --allow-empty -m 'gitmark $NEWTX'"
